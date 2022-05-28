f29e21def56406515cc3c5699dcd4ebf3d00b276
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-28 17:30:14 +0300

    Task Manager - Basic Validation & Try/Catch

--- Task Manager/controller/tasks.js
+++ Task Manager/controller/tasks.js
@@ -1,21 +1,27 @@
 const Task = require(`../models/Task`);
 // An instance of a model is called a document
 
-const getAllTasks = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+const getAllTasks = (req, res) => {
+  res.send('Get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
 };
+
 const updateTask = (req, res) => {
   res.send('update task');
 };
+
 const deleteTask = (req, res) => {
   res.send('delete task');
 };
