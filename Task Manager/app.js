96fb6b5680d9e202bc08d7813a74fb33bd13c56b
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-28 14:34:47 +0300

    Task Manager - General - Routes / Controller

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/v1/tasks', tasks);
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}...|http://localhost:${PORT}`)
+);
