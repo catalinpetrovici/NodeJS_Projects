b070dd39b92d33f33fe49054cae6641dcaa7f319
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-29 21:13:11 +0300

    Store API - Query Object

--- Store API/controllers/products.js
+++ Store API/controllers/products.js
@@ -1,10 +1,28 @@
+const Product = require(`../models/product`);
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
-  //   res.status(200).json({ msg: 'products testing route' });
+  // Basic Find: get all the products with featured true
+  // const products = await Product.find({featured: true,});
+
+  // Basic Find: get the product with the name 'vase table'
+  // const products = await Product.find({ name: 'vase table' });
+
+  res.status(200).json({ msg: products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  // get all the products with => Query Params
+  // const products = await Product.find(req.query);
+
+  // get all the products that have featured true or false
+  // and if there is no featured query we get all the products bcs we have an empty object
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ msg: products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
