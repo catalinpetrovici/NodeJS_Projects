454cfd457e3dec2c176f5a8cf164a06598157f02
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-30 12:09:20 +0300

    JWT Basics - Controllers, Routes

--- JWT - Basics/app.js
+++ JWT - Basics/app.js
@@ -1,9 +1,12 @@
+const PORT = process.env.PORT || 5000;
+
 require('dotenv').config();
 require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -11,16 +14,16 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'));
 app.use(express.json());
 
+app.use('/api/v1', mainRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}...|http://localhost:${PORT}`);
+    });
   } catch (error) {
     console.log(error);
   }
