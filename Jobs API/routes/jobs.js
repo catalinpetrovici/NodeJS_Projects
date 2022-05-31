7d3170a3662a1c181e507521bebf1da51f1cd128
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-31 13:26:06 +0300

    Jobs API - Setup - Controllers & Routes

---
+++ Jobs API/routes/jobs.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { model, modelNames } = require('mongoose');
+const router = express.Router();
+
+const {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require(`../controllers/jobs`);
+
+router.get(`/`, getAllJobs);
+router.get(`/:id`, getJob);
+router.post(`/`, createJob);
+router.patch(`/:id`, updateJob);
+router.delete(`/:id`, deleteJob);
+
+module.exports = router;
