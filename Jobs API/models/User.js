6a22d5be025b15d65c01076f33ea59b391189311
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-31 14:12:04 +0300

    Jobs API - Mongoose Middleware Hash User Password

--- Jobs API/models/User.js
+++ Jobs API/models/User.js
@@ -1,6 +1,8 @@
 // https://mongoosejs.com/docs/schematypes.html#string-validators
+// https://mongoosejs.com/docs/middleware.html#pre
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -23,6 +25,12 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide password'],
     minlength: 6,
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
