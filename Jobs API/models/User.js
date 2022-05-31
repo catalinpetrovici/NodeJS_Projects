acab1dfdbec31714465dede575a42dfc840d53ec
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-31 13:49:12 +0300

    Jobs API - Create User - Basic Setup

--- Jobs API/models/User.js
+++ Jobs API/models/User.js
@@ -18,7 +18,7 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true,
   },
-  name: {
+  password: {
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
