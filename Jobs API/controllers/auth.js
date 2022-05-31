723c3341665065f81dd4e5ee1220c4f5ac77722b
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-05-31 15:32:57 +0300

    Jobs API - Login Controller & Compare Password

--- Jobs API/controllers/auth.js
+++ Jobs API/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors/index');
+const { BadRequestError, UnauthenticatedError } = require('../errors/index');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -10,7 +10,22 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  // check user
+  if (!user) throw new UnauthenticatedError('Invalid Credentials');
+
+  // check & compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) throw new UnauthenticatedError('Invalid Credentials');
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
