b30176a8f1252bc7288427e917caf76b96716283
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-01 16:34:50 +0300

    File Upload - Express File Upload

--- File Upload/controllers/productController.js
+++ File Upload/controllers/productController.js
@@ -1,14 +1,15 @@
-const Product = require('../models/Product');
+const Products = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 
 const createProduct = async (req, res) => {
   console.log(req.body);
-  const product = await Product.create(req.body);
-  res.status(StatusCodes.CREATED).json({ product });
+  const products = await Products.create(req.body);
+  res.status(StatusCodes.CREATED).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.send('list of products');
+  const products = await Products.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
