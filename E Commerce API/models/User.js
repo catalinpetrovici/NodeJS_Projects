eabd06d82112964ce8905056d27a93a76b77abba
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-02 14:56:21 +0300

    E Comm API - User Model & Validator Package

---
+++ E Commerce API/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    // https://mongoosejs.com/docs/validation.html#custom-validators
+    type: String,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
