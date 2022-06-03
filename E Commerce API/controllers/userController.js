5a47faa480b479b8462276e4dd0a6fd304a0182f
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-03 17:33:25 +0300

    E Comm API - get All Users & get Single User

--- E Commerce API/controllers/userController.js
+++ E Commerce API/controllers/userController.js
@@ -1,9 +1,20 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const getAllUsers = async (req, res) => {
-  res.status(200).json({ msg: 'Get All Users' });
+  const users = await User.find({ role: 'user' }).select('-password');
+
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.status(200).json({ msg: 'Get Single User' });
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+
+  if (!user)
+    throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
+
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
