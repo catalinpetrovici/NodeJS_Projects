dabdaa19f3d4ccf1613231ca7beb961d768d9dff
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-04 17:25:40 +0300

    E Comm API - Update Password

--- E Commerce API/controllers/userController.js
+++ E Commerce API/controllers/userController.js
@@ -27,7 +27,22 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.status(200).json({ msg: 'Update User Password', body: req.body });
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword)
+    throw new CustomError.BadRequestError('Please provide both values');
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect)
+    throw new CustomError.UnauthenticatedError('Invalid Credential');
+
+  user.password = newPassword;
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' });
 };
 
 module.exports = {
