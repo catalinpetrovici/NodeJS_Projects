98b423896e1e0dc1c9a8f7917c372564c2b46cf5
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-04 17:41:36 +0300

    E Comm API - UpdateUser - FindOneAndUpdate()

--- E Commerce API/routes/userRoutes.js
+++ E Commerce API/routes/userRoutes.js
@@ -15,7 +15,7 @@ const {
 router.get('/', authenticateUser, authorizePermissions('admin'), getAllUsers);
 
 router.get('/showMe', authenticateUser, showCurrentUser);
-router.patch('/updateUser', updateUser);
+router.patch('/updateUser', authenticateUser, updateUser);
 router.patch('/updateUserPassword', authenticateUser, updateUserPassword);
 
 router.get('/:id', authenticateUser, getSingleUser);
