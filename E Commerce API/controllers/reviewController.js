9cbbfd7a5e4f913992d5d4692340e02105754aa6
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-05 15:11:52 +0300

    E Comm API - Delete/Update Review Controller

--- E Commerce API/controllers/reviewController.js
+++ E Commerce API/controllers/reviewController.js
@@ -46,11 +46,37 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Update Review' });
+  const { id: reviewId } = req.params;
+
+  const { rating, title, comment } = req.body;
+
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review)
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+
+  checkPermissions(req.user, review.user);
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+  await review.save();
+
+  res.status(StatusCodes.OK).json({ review });
 };
 
 const deleteReview = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Delete Review' });
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review)
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+
+  checkPermissions(req.user, review.user);
+  await review.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed.' });
 };
 
 module.exports = {
