e2a6c7a0d71ea445c42cd8350a92fd11bcc01d6c
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-27 17:14:51 +0300

    Auth WorkFlow - Refactor Auth MiddleWare - Access/Refresh Token

--- Auth Workflow/Back End/controllers/authController.js
+++ Auth Workflow/Back End/controllers/authController.js
@@ -58,29 +58,37 @@ const login = async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (!user) throw new CustomError.UnauthenticatedError('Invalid Credential');
+  if (!user) throw new CustomError.UnauthenticatedError('Invalid Credentials');
 
   const isPasswordCorrect = await user.comparePassword(password);
 
   if (!isPasswordCorrect)
-    throw new CustomError.UnauthenticatedError('Invalid Credential');
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
 
   if (!user.isVerified) {
     throw new CustomError.UnauthenticatedError('Please verify your email');
   }
-
   const tokenUser = createTokenUser(user);
 
   // create refresh token
   let refreshToken = '';
   // check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
+  }
 
   refreshToken = crypto.randomBytes(40).toString('hex');
-
   const userAgent = req.headers['user-agent'];
   const ip = req.ip;
-  console.log(userAgent);
-
   const userToken = { refreshToken, ip, userAgent, user: user._id };
 
   await Token.create(userToken);
