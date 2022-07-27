914dbd0aea119688a6836e78bd6f0d46dc0489b8
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-27 18:45:44 +0300

    Auth WorkFlow - Forgot/Reset Password

--- Auth Workflow/Back End/routes/authRoutes.js
+++ Auth Workflow/Back End/routes/authRoutes.js
@@ -5,12 +5,16 @@ const {
   login,
   logout,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 } = require('../controllers/authController');
 const { authenticateUser } = require('../middleware/authentication');
 
 router.post('/register', register);
-router.post('/verify-email', verifyEmail);
 router.post('/login', login);
 router.delete('/logout', authenticateUser, logout);
+router.post('/verify-email', verifyEmail);
+router.post('/reset-password', resetPassword);
+router.post('/forgot-password', forgotPassword);
 
 module.exports = router;
